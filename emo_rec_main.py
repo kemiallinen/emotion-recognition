7b32dc6ad65232098e46593028ae2c2f71fccb69
kemiallinen <35116031+kemiallinen@users.noreply.github.com>
2020-02-03 16:21:43 +0100

    init for keras classification

--- emo_rec_main.py
+++ emo_rec_main.py
@@ -1,68 +1,35 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
+# time for keras
+
+from pandas import read_csv
+from sklearn.model_selection import train_test_split, KFold, cross_val_score
 from sklearn.preprocessing import StandardScaler
-
-from sklearn import model_selection
-from sklearn.tree import DecisionTreeClassifier as dtc
-from sklearn.svm import SVC as svc
-from sklearn.neighbors import KNeighborsClassifier as knc
-from sklearn.linear_model import SGDClassifier as sgdc
-from sklearn.naive_bayes import GaussianNB as gnbc
-from sklearn.neural_network import MLPClassifier as mlpc
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasClassifier
 
 
-def plot_corr_matrices(cm1, cm2):
-    import matplotlib.pyplot as plt
-    fig, (axf, axm) = plt.subplots(1, 2)
-    axf.matshow(cm1 ** 2, interpolation='nearest')
-    axm.matshow(cm2 ** 2, interpolation='nearest')
-
-    for subf in (axf, axm):
-        subf.set_xticks(np.arange(len(cm1)))
-        subf.set_yticks(np.arange(len(cm1)))
-        subf.set_xticklabels(cm1.columns)
-        subf.set_yticklabels(cm1.columns)
-        subf.tick_params(axis='both', labelsize=8)
-        plt.setp(subf.get_xticklabels(), rotation=-45, ha="right",
-                 rotation_mode="anchor")
-    plt.show()
+def create_baseline():
+    model = Sequential()
+    model.add(Dense(60, input_dim=60, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
 
 
-data = pd.read_csv('CompleteData_chunks.csv', sep=';').drop(['Speaker', 'Utterance',
-                                                             'Chunk', 'PSD Filename',
-                                                             'Spectrogram Filename'], axis=1)
-
+data = read_csv('CompleteData_chunks.csv', sep=';').drop(['Speaker', 'Utterance',
+                                                          'Chunk', 'PSD Filename',
+                                                          'Spectrogram Filename'], axis=1)
 sex = 'F'
 scaler = StandardScaler()
-
 data_partial = data[data['Sex'] == sex].drop('Sex', axis=1)
-# corr_matrix_f, corr_matrix_m = data_f.corr(), data_m.corr()
-# plot_corr_matrices(corr_matrix_f, corr_matrix_m)
-
 y = data_partial['EmoState']
 X = scaler.fit_transform(data_partial.drop('EmoState', axis=1))
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=71)
 
-models = (('DTC', dtc()), ('SVM', svc(C=10)),
-          ('KNN', knc(n_neighbors=10)), ('SGDC', sgdc()),
-          ('GNBC', gnbc()), ('MLPC', mlpc(max_iter=1000, learning_rate='adaptive')))
-results = []
-names = []
 seed = 13
 scoring = 'accuracy'
 
-for name, model in models:
-    kfold = model_selection.KFold(n_splits=10, random_state=seed, shuffle=True)
-    cv_results = model_selection.cross_val_score(model, X_train, y_train,
-                                                 cv=kfold, scoring=scoring)
-    results.append(cv_results)
-    names.append(name)
-    print('{}: {} ({})'.format(name, round(cv_results.mean(), 2), round(cv_results.std(), 2)))
-
-fig = plt.figure()
-ax = fig.add_subplot(111)
-plt.boxplot(results)
-ax.set_xticklabels(names)
-plt.show()
+estimator = KerasClassifier(build_fn=create_baseline, epochs=100, batch_size=5, verbose=0)
+kfold = KFold(n_splits=10, random_state=seed, shuffle=True)
+cv_results = cross_val_score(estimator, X_train, y_train, cv=kfold, scoring=scoring)
+print('Dense: {} ({})'.format(round(cv_results.mean(), 2), round(cv_results.std(), 2)))
